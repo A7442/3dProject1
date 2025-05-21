2ba33d85628994479a93ba5d358055c3f983e6a9
A7442 <parkjuna1121@naver.com>
2025-05-21 23:00:56 +0900

    feat : 상호작용 추가

--- Assets/Scripts/Item/PlayerInteraction.cs
+++ Assets/Scripts/Item/PlayerInteraction.cs
@@ -11,24 +11,23 @@ public class PlayerInteraction : MonoBehaviour
     
     private IInteractable _curInteractable;
     private Camera _camera;
-    private float _checkRate = 0.05f;
-    private float _maxCheckDistance = 3.0f;
+    private float _checkRate = 0.1f;
+    private float _maxCheckDistance = 2.5f;
     private float _lastCheckTime = 0f;
 
     private void Start()
     {
         _camera = Camera.main;
+        promptText.gameObject.SetActive(false);
     }
 
     private void Update()
     {
-
         if (Time.time - _lastCheckTime < _checkRate)
         {
             _lastCheckTime = Time.time;
             Ray ray = _camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
             RaycastHit hit;
-
             if (Physics.Raycast(ray, out hit, _maxCheckDistance, layerMask))
             {
                 if (hit.collider.gameObject != curInteractGameObject)
@@ -37,12 +36,12 @@ public class PlayerInteraction : MonoBehaviour
                     _curInteractable = hit.collider.GetComponent<IInteractable>();
                     SetPromptText();
                 }
-                else
-                {
-                    curInteractGameObject = null;
-                    _curInteractable = null;
-                    promptText.gameObject.SetActive(false);
-                }
+            }
+            else
+            {
+                curInteractGameObject = null;
+                _curInteractable = null;
+                promptText.gameObject.SetActive(false);
             }
         }
     }
